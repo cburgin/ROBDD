4140c7b944a3b08fac75ba118df20ad09fd56142
Colin Burgin <cburgin707@gmail.com>
2017-02-20 18:47:22 -0500

    changed file name

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
 # Main for interacting with the ROBDD
+# Libraries:
+#   -pyeda, a python EDA library. It provides a lot of functionality, I am only
+#   using its expression conversion functionality so that I don't have to roll my
+#   own using lex, flex, etc.
 
 import argparse
+from pyeda.boolalg.expr import exprvar, expr
 
 def main():
     #Parse the command line arguments provided at run time.
@@ -10,8 +15,10 @@ def main():
     parser.add_argument('-b', '--boolean_expr', dest='input_expr', metavar='B',
                         type=str, nargs='?',help='Provide a boolean expression')
 
+    # Parse the input arguments
     args = parser.parse_args()
-    print(args)
+
+    print(expr(args.input_expr))
 
 if __name__ =='__main__':
     main()
--- parse.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-# Parse a Boolean Expression
-
-from pyeda.boolalg.expr import exprvar, expr
-
-print(expr("A | B"))
